2f6101040a4bcfc1e18c58861c2844cc5dd7eaac
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-24 17:59:58 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+console.log(3
+    +3) // code readability should be high
+console.log("Paarth");
+let name="Paarth"
+let age= 18
+let isLoggedIn= false
+let state
+
+//number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undefined means value not assigned
+// symbol => Uniqueness
+ // object
+ console.log(typeof "Paarth")
+ console.log(typeof age);
+ console.log(typeof null); //object
+ console.log(typeof undefined); //undefined
+ 
