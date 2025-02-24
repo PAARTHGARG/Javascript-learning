b3182b03c762b266fb7b3cbf1e9b0f46d7f4efe4
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-24 19:39:42 +0000

    Conversions

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,10 +1,10 @@
 let score = true;
-console.log(typeof score)
-console.log(typeof (score))
+//console.log(typeof score)
+//console.log(typeof (score))
  
 let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 /* value before vs value after conversion
   "33" => 33 
 
@@ -13,7 +13,37 @@ true => 1; false => 0
 */
 let isLoggedIn=1
 let booleanIsLoggedIn= Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 /* 1->True 0->False
-"" -> False "Paarth" -> True */
+"" -> False "Paarth" -> True */ 
+
+//OPERATIONS
+
+let value = 3
+let negVal= -value
+//console.log(negVal)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/3)
+//console.log(2%3)
+let str1="Hello"
+let str2="Paarth"
+let str3= str1+str2
+//console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log(1+"2"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+console.log(+true)
+console.log(+"")
+let num1,num2,num3
+num1=num2=num3= 2+2
+console.log(num3) 
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter)
+
