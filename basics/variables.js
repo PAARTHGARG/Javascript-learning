ef85039eebaca880d15f4953245cb6857fc8e87b
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-24 16:41:04 +0000

    Variables

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId=11234
+var accountEmail= "Paarth@gmail.com"
+let accountPassword= 12345
+accountCity="Jaipur"
+let accountState;
+//dont use var
+//const value cant be changed otherwise it shows an error
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
