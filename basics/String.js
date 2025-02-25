e5c269e8db2a8dae66c7c03584592049937a9ed1
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-25 12:23:58 +0000

    Strings

---
+++ basics/String.js
@@ -0,0 +1,24 @@
+const name="Paarth"
+const repoCount = 50
+// console.log(name + ' '+ repoCount+ " Garg"); // Old method to do so
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); //New method
+const gameName = new String("PaarthGarg")
+//const gameName2 = new String("Raja")
+//console.log(gameName[2])
+//console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(8))
+// console.log(gameName.indexOf('a'))
+//let newString= gameName.concat(gameName2)
+let newString= gameName.substring(3,6)
+//console.log(newString);
+let newString2= gameName.slice(-10,3)
+//console.log(newString2)
+let String1 = "   Paarth   "
+//console.log(String1)
+//console.log(String1.trim())
+const url = "https://Paarth.com/Paarth%20Garg"
+//console.log(url.replace("%20","-"))
+//console.log(url.includes("%20"))
+console.log(url.split("/"))
