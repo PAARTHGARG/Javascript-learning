eab699a908feeb10d12227a9306c1f79165fd1a6
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-26 18:14:40 +0000

    Scope

---
+++ basics3/Scope.js
@@ -0,0 +1,14 @@
+
+//var c= 200
+let a = 100
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Inner: ",a)
+} 
+
+ console.log(a);
+// console.log(b);
+//console.log(c); // here c will not be 200 but 30
