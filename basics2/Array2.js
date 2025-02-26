f0f94fa54d3c486b5fec4e0fd9a5727c8774b38d
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-26 07:28:19 +0000

    Array2

---
+++ basics2/Array2.js
@@ -0,0 +1,25 @@
+const marvel_heroes= ["thor","ironman","spiderman"]
+const dc_heroes= ["superman","flash","batman"]
+const hc_heroes= ["aa","bb","cc"]
+
+//marvel_heroes.push(dc_heroes)
+// console.log(marvel_heroes)
+//console.log(marvel_heroes[3][1])
+
+// const allHeroes= marvel_heroes.concat(dc_heroes,hc_heroes)
+// console.log(allHeroes)
+
+// const allHeroes= [...marvel_heroes,...dc_heroes]
+// console.log(allHeroes);
+ 
+//const anotherArray = [1,2,3,[4,5,6],7,[8,9,[10,11],12],13]
+//console.log(anotherArray.flat(Infinity));
+
+// console.log(Array.isArray("Paarth"))
+// console.log(Array.from("Paarth"))
+// console.log(Array.from({name:"Paarth"})) //returns empty [] INTERESTING
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
