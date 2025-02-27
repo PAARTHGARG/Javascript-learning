12def56f31d40046fac753b17f3500a3a4b41313
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-02-27 11:27:25 +0000

    IIFE

---
+++ basics3/IIFE.js
@@ -0,0 +1,12 @@
+// function chai(){
+//     console.log("DB CONNECTED")
+// }
+// chai() // Normal function
+
+(function chai(){
+    console.log(`DB CONNECTED`)
+})()
+
+// ((name)=>{
+//          console.log(`DB CONNECTED ${name}`)
+// }   )("paarth")
