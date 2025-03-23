71ecd3470114d00782b82b2f86054307732289c1
PAARTHGARG <90719768+PAARTHGARG@users.noreply.github.com>
2025-03-23 11:40:00 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,18 @@
+// let index=0
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index +=2
+// }
+// let myArray=["batman","superman","spiderman"]
+// i=0
+// while(i<myArray.length){
+//     console.log(`Value of array is ${myArray[i]}`);
+//     i+=1
+    
+// }
+// do while
+let score = 6
+do {
+    console.log(`Value of score is ${score}`);
+    score++
+} while (score<=5);
